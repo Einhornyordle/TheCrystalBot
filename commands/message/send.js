51f4a73534b8b2ac2ba8f17afefb23f461d07621
Einhornyordle <47315090+Einhornyordle@users.noreply.github.com>
2024-11-20 02:02:10 +0100

    Allow image-only messages

--- commands/message/send.js
+++ commands/message/send.js
@@ -9,17 +9,24 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 		.addStringOption(option =>
 			option.setName('content')
-				.setDescription('The content of the message')
-				.setRequired(true))
+				.setDescription('The content of the message'))
 		.addAttachmentOption(option =>
 			option.setName('attachment')
-				.setDescription('A optional attachment to send with the message')
-				.setRequired(false)),
+				.setDescription('A optional attachment to send with the message')),
 	async execute(interaction) {
+		const options = {send: false};
+		if (interaction.options.getString('content')) {
+			options.content = interaction.options.getString('content');
+			options.send = true;
+		}
 		if (interaction.options.getAttachment('attachment')) {
-			await interaction.reply({ content: interaction.options.getString('content'), files: [interaction.options.getAttachment('attachment')] });
+			options.files = [interaction.options.getAttachment('attachment')];
+			options.send = true;
+		}
+		if (options.send) {
+			await interaction.reply(options);
 		} else {
-			await interaction.reply({ content: interaction.options.getString('content') });
+			await interaction.reply({ content: 'At least one of the optional parameters is required!', ephemeral: true });
 		}
 	}
 };
